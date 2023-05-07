a56da2e9770557e553abb78439aff83add22ec86
DzikStar <dawid0mila@gmail.com>
2023-05-07 21:52:24 +0200

    Comments Changes (more visible and another structure changes)

--- main.py
+++ main.py
@@ -7,27 +7,25 @@
 import discord # pip install py-cord - not discordpy
 import json
 
-# Loading all configuration files which will be used by our client and bot
-with open('userConfig.json') as userConfigFile:
+## Loading all configuration files which will be used by our client and bot
+with open('config/userConfig.json') as userConfigFile:
     userConfig = json.load(userConfigFile)
 
-with open('clientConfig.json') as clientConfigFile:
+with open('config/clientConfig.json') as clientConfigFile:
     clientConfig = json.load(clientConfigFile)
 
-# Def of all main and important variables which will be used later in our code
+## Def of all main and important variables which will be used later in our code
 discordAuthToken = str(userConfig['discordAuth'])
 channelSupportChanges = str(clientConfig['secondsBetweenContentNextVerify'])
 channelDatamining = str(clientConfig['secondsBetweenContentNextVerify'])
 verifyCooldown = int(clientConfig['secondsBetweenContentNextVerify'])
 
-# Idk what is that thing doing but okay :troll:
+## Idk what is that thing doing but okay :troll:
 bot = discord.Bot()
 
-@bot.slash_command()
-async def hello(ctx, name: str = None):
-    ## Example for documentation
-    ## Every command should have docs before creation
-    name = name or ctx.author.name
-    await ctx.respond(f"Hello {name}!")
+## Place for slash commands
 
+## Place for Bot tasks
+
+# goofy ooh ahhh bot startup
 bot.run(discordAuthToken)
