d0c542513a2a7af61fbff11669a66d814920eaac
DzikStar <dawid0mila@gmail.com>
2023-05-08 13:49:25 +0200

    Added looping task and simple slash command

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 import discord # pip install py-cord - not discordpy
 import json
 import os
+import time
+import asyncio
 
 ## Loading all configuration files which will be used by our client and bot
 config_path = os.path.join(os.path.dirname(__file__), 'config', 'userConfig.json')
@@ -30,8 +32,16 @@ verifyCooldown = int(clientConfig['secondsBetweenContentNextVerify'])
 bot = discord.Bot()
 
 ## Place for slash commands
+@bot.slash_command(name = "testing", description = "super duper testing command")
+async def hello(ctx):
+    await ctx.respond("Hey! We're actually testing some stuff with our bot. If you are interested in development of our bot then you can be interested :troll:")
 
 ## Place for Bot tasks
+async def supportTrackerTask():
+    while True:
+        print("Message") ## Debug stuff
+        await asyncio.sleep(verifyCooldown)
 
 # goofy ooh ahhh bot startup
+bot.loop.create_task(supportTrackerTask()) ## Support Changes
 bot.run(discordAuthToken)
