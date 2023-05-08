f2100ec1dcee173c534867500c2bdb8aea1f3790
DzikStar <dawid0mila@gmail.com>
2023-05-08 15:33:04 +0200

    Added command logger basis

--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 ## Description:
 ## This is main.py while which we will be using for running this bot.
 
-import discord # pip install py-cord - not discordpy
+import discord ## pip install py-cord - not discordpy
 import json
 import os
 import time
 import asyncio
-import colorama
+from utils import workerLogs
 
 ## Loading all configuration files which will be used by our client and bot
 config_path = os.path.join(os.path.dirname(__file__), 'config', 'userConfig.json')
@@ -20,13 +20,15 @@ config_path = os.path.join(os.path.dirname(__file__), 'config', 'clientConfig.js
 with open(config_path) as clientConfigFile:
     clientConfig = json.load(clientConfigFile)
 
-## Idk what is that thing doing but okay :troll:
+## Declared discord.Bot() as a `bot`, may be changes in the future
 bot = discord.Bot()
 
 ## Place for slash commands
 @bot.slash_command(name = "testing", description = "super duper testing command")
-async def hello(ctx):
+async def testing(ctx):
+    ## This command is replyign with a message to a user, nothing more. Using 
     await ctx.respond("Hey! We're actually testing some stuff with our bot. If you are interested in development of our bot then you can be interested :troll:")
+    print(workerLogs.userUsedCommand(member=str(ctx.author.name), commandName=str('testing'), executeType=int(2)))
 
 ## Place for Bot tasks
 async def supportTrackerTask():
@@ -34,6 +36,6 @@ async def supportTrackerTask():
         print("Message") ## Debug stuff
         await asyncio.sleep(clientConfig['secondsBetweenContentNextVerify'])
 
-# goofy ooh ahhh bot startup
+## Client Startup
 bot.loop.create_task(supportTrackerTask()) ## Support Changes
 bot.run(userConfig['discordAuth'])
---
+++ utils/workerLogs.py
@@ -0,0 +1,27 @@
+## Author: @DzikStar
+## Contributors: @DzikStar
+##
+## Description:
+## This util is used for generating fancy logs messages in the hoster console
+## Will be used for client events, commands and bot tasks
+
+from colorama import Fore
+
+def userUsedCommand(member, commandName, executeType):
+    ## This function is used to return to hoster console logs message about command usage (actually lowpoly version without metrics)
+    ## Variables Used There:
+    ##   - string: member: User name of the user which used the command
+    ##   - string: commandName: Codename of the command
+    ##   - int: executeType: Information about that if command was used as a slash or prefix command
+    ##     * 1 = PREFIX COMMAND
+    ##     * 2 = SLASH COMMAND
+
+    ## Setting up convertion
+    if executeType == 1:
+        commandExecutionMethod = str('prefix command')
+    elif executeType == 2:
+        commandExecutionMethod = str('slash command')
+
+    logMessage = f'{Fore.CYAN}[COMMAND USAGE]{Fore.RESET} {Fore.YELLOW}{member}{Fore.RESET} used {Fore.BLUE}{commandName}{Fore.RESET} command as a {Fore.BLUE}{commandExecutionMethod}{Fore.RESET}.'
+    return logMessage
+    del executeType, commandExecutionMethod, member, commandName, logMessage
